bdf32fd36e10f23e801120ce280576440d9176c1
adwaitpawar <adwaitpawar@gmail.com>
2024-01-31 19:37:20 +0530

    first commit

---
+++ dbo/Tables/users.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[users] (
+    [user_id]   INT           NOT NULL,
+    [username]  VARCHAR (50)  NOT NULL,
+    [email]     VARCHAR (100) NULL,
+    [birthdate] DATE          NULL,
+    [is_active] BIT           DEFAULT ((1)) NULL,
+    PRIMARY KEY CLUSTERED ([user_id] ASC),
+    UNIQUE NONCLUSTERED ([email] ASC)
+);
+
